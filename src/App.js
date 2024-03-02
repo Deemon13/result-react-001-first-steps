15aeec5ca2ab172d6256c2dcbf3708843773e2a7
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2024-03-02 23:22:12 +0300

    add editor config, year & comments of programming style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
+// imperative style
 import logo from './logo.svg';
+// imperative style
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+// imperative style
+export const App = () => {
+	// declaration style
+	const currentYear = new Date().getFullYear();
+	// imperative style
+	return (
+		// all HTML elements - declaration style
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<p>{currentYear}</p>
+			</header>
+		</div>
+	);
+};
