ca03be6e185c8cc4d6bf05bf0e0fdc05241a4666
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2024-03-02 23:37:32 +0300

    change jsx to createElement

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 // imperative style
+import { createElement } from 'react';
+// imperative style
 import logo from './logo.svg';
 // imperative style
 import './App.css';
@@ -9,23 +11,37 @@ export const App = () => {
 	const currentYear = new Date().getFullYear();
 	// imperative style
 	return (
-		// all HTML elements - declaration style
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{currentYear}</p>
-			</header>
-		</div>
+		// all elements - declaration style
+		createElement(
+			'div',
+			{ className: 'App' },
+			createElement(
+				'header',
+				{ className: 'App-header' },
+				createElement('img', {
+					src: logo,
+					className: 'App-logo',
+					alt: 'logo',
+				}),
+				createElement(
+					'p',
+					null,
+					'Edit ',
+					createElement('code', null, 'src/App.js'),
+					' and save to reload.',
+				),
+				createElement(
+					'a',
+					{
+						className: 'App-link',
+						href: 'https://reactjs.org',
+						target: '_blank',
+						rel: 'noopener noreferrer',
+					},
+					'Learn React',
+				),
+				createElement('p', null, currentYear),
+			),
+		)
 	);
 };
